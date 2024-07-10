9c818c03c7f99f0be2bf5c5c01fd0539b9fb3278
Paulo-D2000 <highvoltagegenerator@hotmail.com>
2024-07-10 15:36:45 -0300

    Updated Networking & cmake for windows

--- src/Networking.cpp
+++ src/Networking.cpp
@@ -16,12 +16,10 @@ SocketSource<OT, net>::SocketSource(uint16_t Port, size_t PacketSize, bool clien
     CheckError(m_sockfd);
     //fcntl(m_sockfd, F_SETFL, O_NONBLOCK);
 
-    m_addr = {
-    .sin_family = AF_INET,
-    .sin_port = htons(Port),
-    .sin_addr = {.s_addr = INADDR_ANY},
-    .sin_zero = {0}
-    };
+    m_addr.sin_family = AF_INET;
+    m_addr.sin_port = htons(Port);
+    m_addr.sin_addr.s_addr = INADDR_ANY;
+    memset(m_addr.sin_zero, 0, 8);
 
     m_buffer = std::make_unique<U8[]>(m_pktsize);
     if(sizeof(OT) == 2){ // I16
@@ -84,7 +82,7 @@ size_t SocketSource<F32, UDP>::work(std::vector<F32>& output){
 
 template <typename OT, NetworkConnection net>
 size_t SocketSource<OT, net>::work_complex(std::vector<CF32>& output){
-    int nread = recv(m_sockfd, &m_buffer[0], m_pktsize, 0);
+    int nread = recv(m_sockfd, (char*)&m_buffer[0], std::min(4*output.size(), m_pktsize), 0);
     if(nread == -1){
         return 0;
     }
@@ -99,7 +97,7 @@ size_t SocketSource<OT, net>::work_complex(std::vector<CF32>& output){
 
 template <typename OT, NetworkConnection net>
 size_t SocketSource<OT, net>::work_float(std::vector<F32>& output){
-    int nread = recv(m_sockfd, &m_buffer[0], m_pktsize, 0);
+    int nread = recv(m_sockfd, (char*)&m_buffer[0], std::min(2*output.size(), m_pktsize), 0);
     if(nread == -1){
         return 0;
     }
@@ -113,7 +111,7 @@ size_t SocketSource<OT, net>::work_float(std::vector<F32>& output){
 
 template <typename OT, NetworkConnection net>
 size_t SocketSource<OT, net>::work_s16(std::vector<I16>& output){
-    int nread = recv(m_sockfd, &output[0], m_pktsize, 0);
+    int nread = recv(m_sockfd, (char*)&output[0], std::min(output.size(), m_pktsize), 0);
     if(nread == -1){
         return 0;
     }
